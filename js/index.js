9315b252f8ccfb0ab93ca92dac7d2eb52bd9a5af
Aliaksei Karzhou <alexius90e@gmail.com>
2024-08-30 21:58:44 +0300

    feat: added modals

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ function showAlert(alert) {
 
   if ((alerts, alertsItem)) {
     alertsItem.innerText = String(alert);
-    console.log(alertsItem.innerText)
+    console.log(alertsItem.innerText);
     alerts.classList.add('active');
     alertsItem.classList.add(`alerts__item_${alert}`);
 
@@ -32,5 +32,47 @@ if (walletInfoCode) {
       navigator.clipboard.writeText(walletInfoCode.dataset.code);
       showAlert(alerts.copied);
     }
+  });
+}
+
+const modals = {
+  withdraw: 'modal_complete-withdraw',
+  swap: 'modal_complete-swap',
+  points: 'modal_points',
+};
+
+const modalElems = document.querySelectorAll('.modal');
+
+function showModal(modalClassName) {
+  const modalElem = document.querySelector(`.${modalClassName}`);
+  if (modalElem) modalElem.classList.add('active');
+}
+
+function closeModal(modal) {
+  modal.classList.remove('active');
+}
+
+modalElems.forEach((modal) => {
+  modal.addEventListener('click', (event) => {
+    const isModal = event.target === event.currentTarget;
+    const isButton = event.target.classList.contains('button');
+
+    if (isModal || isButton) closeModal(modal);
+  });
+});
+
+const walletSwapForm = document.querySelector('.wallet__swap-form');
+const walletTokensCardPointsInfoButton = document.querySelector('.wallet__tokens-card-points-info');
+
+if (walletSwapForm) {
+  walletSwapForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    showModal(modals.swap);
+  });
+}
+
+if (walletTokensCardPointsInfoButton) {
+  walletTokensCardPointsInfoButton.addEventListener('click', () => {
+    showModal(modals.points);
   });
 }
